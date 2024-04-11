b981888a3b1b1f086b89a445c0cb4935ee43763f
kath424 <acostak424@gmail.com>
2024-04-11 03:45:33 +0000

    Ejercicios array JS  H-5.1 - 4geeks

--- exercises/04.3-Add_items_to_array/app.js
+++ exercises/04.3-Add_items_to_array/app.js
@@ -1,5 +1,12 @@
 let arr = [4,5,734,43,45];
 
 // Your code here, use the push() function and the 2 Math functions
+function numeroAleatorio(){
+    let numero = Math.floor(Math.random() * 10)
+    return numero
+}
+
+arr.push(numeroAleatorio())
+arr.push(numeroAleatorio())
 
 console.log(arr);
